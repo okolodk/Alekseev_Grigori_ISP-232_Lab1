c4b98fa6d1e23f4af9033213e723594e32b5520c
Nadia Rykhlyuk <nadigar1506@mail.ru>
2025-09-14 13:42:52 +0300

    Задание 3

--- src/laba.kt
+++ src/laba.kt
@@ -1,11 +1,16 @@
 fun main() {
 //Задание 1
     /*val a = "Library_of_Ruina"
-val b = 11
-val c = 3.14
-val d = "Г"
-println(a)
-println(b)
-println(c)
-println(d)*/
+    val b = 11
+    val c = 3.14
+    val d = "Г"
+    println(a)
+    println(b)
+    println(c)
+    println(d)*/
+    //Задание 3
+//    val pi = 3.14
+//    val rad = 5
+//    println("${pi * rad.toDouble() * rad.toDouble()}")
+
 }
